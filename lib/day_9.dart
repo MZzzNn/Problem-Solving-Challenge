ad889938e1c16d28fa08360fd6166edc8c2b9245
mazen mohamed <mazenn770@gmail.com>
2022-04-22 14:40:01 +0200

    day 9

--- lib/day_9.dart
+++ lib/day_9.dart
@@ -1,22 +1,12 @@
+import 'dart:math' as math;
 class Day9Solution {
   int maxSubArray(List<int> nums) {
-
-    var result = 0;
-    for (var i = 0; i < nums.length; i++) {
-      if (0 < nums[i]) {
-        if (0 > nums[i + 1]) {
-          continue;
-        } else {
-          result += nums[i];
-        }
-      }
-
-
+    var result = nums[0];
+    //[-2,1,-3,4,-1,2,1,-5,4]
+    for (var i = 1; i < nums.length; i++) {
+      nums[i] = math.max(nums[i], nums[i] + nums[i - 1]);
+      result = math.max(result, nums[i]);
     }
-
-
-
-
     return result;
   }
 }
