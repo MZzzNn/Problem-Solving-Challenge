8335770ef29293cf202b913f8ee5cbba92379b92
mazen mohamed <mazenn770@gmail.com>
2022-05-02 17:46:53 +0200

    day 13

--- lib/day_13.dart
+++ lib/day_13.dart
@@ -1,7 +1,11 @@
 class Day13Solution {
   void merge(List<int> nums1, int m, List<int> nums2, int n) {
-    List<int> result = [];
-
-    print('Day 13 : $result');
+    List<int> merged = [];
+    merged = merged.toList()
+      ..addAll(nums1)
+      ..addAll(nums2)
+      ..removeWhere((element) => element == 0)
+      ..sort();
+    print('Day 13 : $merged');
   }
 }
