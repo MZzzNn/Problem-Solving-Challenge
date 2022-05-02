c5baa110ba62b25dd2d66dbf970cc9a96054969d
mazen mohamed <mazenn770@gmail.com>
2022-05-02 17:47:42 +0200

    day 14

--- lib/day_14.dart
+++ lib/day_14.dart
@@ -1,25 +1,22 @@
 class Day14Solution {
   int singleNumber(List<int> nums) {
-     var result = 0;
-    // if number repeats only twice you can use this solution
+    var result = 0;
+    /// TODO: if number repeats only twice you can use this solution
     // for (int num in nums) {
     //   result ^= num;
     // }
 
-     // if number repeats more than twice you can use this solution
-     for (int num in nums) {
-       if(count(nums,num) == 1) {
-         result =  num;
-       }
-     }
-
-
-     return result;
+    /// TODO: if more than number repeats more than twice you can use this solution
+    for (int num in nums) {
+      if (count(nums, num) == 1) result = num;
+    }
+    return result;
   }
-  int count(List<int> listNumbers , int num) {
-    var i =0;
+
+  int count(List<int> listNumbers, int num) {
+    var i = 0;
     listNumbers.forEach((element) {
-      if(element == num) i++;
+      if (element == num) i++;
     });
     return i;
   }
