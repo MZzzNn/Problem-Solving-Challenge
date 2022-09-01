163a341588a81c5aec554e02e748e72eeabfa34b
Mazen Mohamed <55028517+mazen-mo7amed@users.noreply.github.com>
2022-09-02 00:38:32 +0200

    Update day_5.dart

--- lib/day_5.dart
+++ lib/day_5.dart
@@ -1,12 +1,5 @@
 class Day5Solution {
   int lengthOfLastWord(String s) {
-    int result = 0;
-    s.trim().split(' ')
-      ..removeWhere((e) => e.isEmpty)
-      ..forEach((element) {
-        result = result > element.length ? result : element.length;
-      });
-
-    return result;
+    return s.trim().split(' ').last.length;
   }
 }
