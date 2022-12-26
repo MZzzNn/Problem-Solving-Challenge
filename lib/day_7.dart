018c9a9945382f012250020bbb522dd1763b47b4
Mazen <mazenn770@gmail.com>
2022-12-27 01:21:08 +0200

    sort problems

--- lib/day_7.dart
+++ lib/day_7.dart
@@ -1,10 +1,8 @@
 class Day7Solution {
   bool isAnagram(String s, String t) {
     if (s.length != t.length) return false;
-    List<String> sList = s.split('');
-    List<String> tList = t.split('');
-    sList.sort();
-    tList.sort();
+    List<String> sList = s.split('')..sort();
+    List<String> tList = t.split('')..sort();
     for (var i = 0; i < sList.length; i++) {
       if (sList[i] != tList[i]) return false;
     }
