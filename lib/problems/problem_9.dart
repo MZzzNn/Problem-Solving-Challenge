67a61ca60b75f8794468577eda8a395a500b01fe
Mazen <mazenn770@gmail.com>
2023-01-01 22:54:49 +0200

    add Pascal's Triangle problem

--- lib/problems/problem_9.dart
+++ lib/problems/problem_9.dart
@@ -1,6 +1,17 @@
 class Problem9Solution {
   List<List<int>> generate(int numRows) {
-
-    return [];
+    List<List<int>> result = <List<int>>[];
+    for (int i = 0; i < numRows; i++) {
+      List<int> row = <int>[];
+      for (var j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          row.add(1);
+        } else {
+          row.add(result[i - 1][j - 1] + result[i - 1][j]);
+        }
+      }
+      result.add(row);
+    }
+    return result;
   }
 }
