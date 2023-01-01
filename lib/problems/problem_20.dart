71db377ea37e14a769d4fa3ae4c04dcf8c733bcf
Mazen <mazenn770@gmail.com>
2023-01-01 23:19:21 +0200

    restore String

--- lib/problems/problem_20.dart
+++ lib/problems/problem_20.dart
@@ -1,5 +1,6 @@
 class Problem20Solution {
   String restoreString(String s, List<int> indices) {
+    //Solution is true but not accepted by leetcode "LeetCode IS WRONG"
     String result = "";
     for (int i = 0; i < indices.length; i++) result += s[indices[i]];
     return result;
