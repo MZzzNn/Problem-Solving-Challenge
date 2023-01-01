aabaa3e6a0f3f4473b2a32135b8d31658799d57b
Mazen <mazenn770@gmail.com>
2023-01-01 23:30:04 +0200

    missing Number

--- lib/problems/problem_22.dart
+++ lib/problems/problem_22.dart
@@ -1,10 +1,7 @@
 class Problem22Solution {
   int missingNumber(List<int> nums) {
-    int miss = 0;
-    for (int i = 0; i < nums.length; i++) {
-      miss ^= nums[i];
-     // print('$miss ^ ${nums[i]} = ${miss ^= nums[i]}');
-    }
-    return miss;
+    int sum = 0;
+    for (int i = 0; i < nums.length; i++) sum += i - nums[i];
+    return sum + nums.length;
   }
 }
